523ea238c12424fb56dd611409c738da2805db79
PauApps <appspauapps@gmail.com>
2019-06-15 11:51:56 +0200

    Java to Kotlin

--- app/src/main/java/com/pauapps/pau/projectefoca/Calendar.kt
+++ app/src/main/java/com/pauapps/pau/projectefoca/Calendar.kt
@@ -3,6 +3,7 @@ package com.pauapps.pau.projectefoca
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
+//Develop a calendar view
 class Calendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
