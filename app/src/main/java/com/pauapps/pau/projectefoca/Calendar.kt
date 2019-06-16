79fbaeb48fc9a8d966df63e31f169e1c640666bd
PauApps <appspauapps@gmail.com>
2019-06-16 19:26:30 +0200

    Calendar

--- app/src/main/java/com/pauapps/pau/projectefoca/Calendar.kt
+++ app/src/main/java/com/pauapps/pau/projectefoca/Calendar.kt
@@ -1,26 +1,105 @@
 package com.pauapps.pau.projectefoca
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.CalendarView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.*
+import kotlinx.android.synthetic.main.calendar.*
 
 //Develop a calendar view
 class Calendar : AppCompatActivity() {
+    var daySelect: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.calendar)
 
         val calendarView = findViewById<CalendarView>(R.id.calendar)
-        calendarView?.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+        calendarView?.setOnDateChangeListener { view, year, month, day ->
+            openDay()
+            daySelect = day.toString() + "/" + (month + 1) + "/" + year
+            val msg = "Selected date is " + day + "/" + (month + 1) + "/" + year
             Toast.makeText(this@Calendar, msg, Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    @SuppressLint("InflateParams", "ObsoleteSdkInt")
+    private fun openDay() {
+        // Initialize a new layout inflater instance
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        // Inflate a custom view using layout inflater
+        val view = inflater.inflate(R.layout.day, null)
+
+        // Initialize a new instance of popup window
+        val popupWindow = PopupWindow(
+                view, // Custom view to show in popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT, // Width of popup window
+                LinearLayout.LayoutParams.WRAP_CONTENT // Window height
+        )
+        popupWindow.isOutsideTouchable = true
+        popupWindow.isFocusable = true
+
+
+        // Set an elevation for the popup window
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10.0F
+        }
+
+
+        // If API level 23 or higher then execute the code
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Create a new slide animation for popup window enter transition
+            val slideIn = Slide()
+            slideIn.slideEdge = Gravity.TOP
+            popupWindow.enterTransition = slideIn
+
+            // Slide animation for popup window exit transition
+            val slideOut = Slide()
+            slideOut.slideEdge = Gravity.RIGHT
+            popupWindow.exitTransition = slideOut
+
+        }
+
+
+        // Get the widgets reference from custom view
+        val tv = view.findViewById<TextView>(R.id.text_view)
+        val buttonPopup = view.findViewById<Button>(R.id.button_popup)
+
+        // Set click listener for popup window's text view
+        tv.setOnClickListener {
+            // Change the text color of popup window's text view
+            tv.setTextColor(Color.RED)
+        }
+
+        // Set a click listener for popup's button widget
+        buttonPopup.setOnClickListener {
+            // Dismiss the popup window
+            popupWindow.dismiss()
+        }
+
+
+        // Set a dismiss listener for popup window
+        popupWindow.setOnDismissListener {
+            Toast.makeText(applicationContext, "Popup closed", Toast.LENGTH_SHORT).show()
+        }
+
+
+        // Finally, show the popup window on app
+        TransitionManager.beginDelayedTransition(calendar_layout)
+        popupWindow.showAtLocation(
+                calendar_layout, // Location to display popup window
+                Gravity.CENTER, // Exact position of layout to display popup
+                0, // X offset
+                0 // Y offset
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -47,7 +126,7 @@ class Calendar : AppCompatActivity() {
                 startActivity(intent)
                 //txt.setText("MENU");
             }
-            R.id.action_compra ->{
+            R.id.action_compra -> {
 
             }
         }
